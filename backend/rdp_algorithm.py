0ccf17065ebfac769f13458a7c1912604128d3fe
Eddy Pan <eddydotpan@gmail.com>
2024-04-03 11:07:51 -0400

    Add saving critical images to folder in backend for rdp_algorithm.py

--- backend/rdp_algorithm.py
+++ backend/rdp_algorithm.py
@@ -3,15 +3,18 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
+import shutil
+import os
+from pathlib import Path
 import rdp
 
 TAR_FILE_NAME = (
-    "training_ua-5ad6b6714d5e52c97ca53c47f38b9655_Test_3-13-occamlab-3rd_floor"
+    "training_ua-9843530f4d1fbbdbd0cc26bb3e655da1_HAL_lab_to_206"  # don't include .tar
 )
 EXTRACTED_IMAGES = (
     f"backend/.cache/firebase_data/{TAR_FILE_NAME}/extracted/localization-video/"
 )
-critical_images = {}
+critical_images = {}  # {index of image} : timestamp (float)
 
 if __name__ == "__main__":
     downloader_1 = FirebaseDownloader(
@@ -31,7 +34,7 @@ if __name__ == "__main__":
 
     abs_positions = rdp.rdp(positions, 0.6, return_mask=True)
 
-    abs_timestamp = []
+    # abs_timestamp = []
     indexes = []
     for idx, abs_position in enumerate(abs_positions):
         if abs_position:
@@ -39,11 +42,20 @@ if __name__ == "__main__":
             critical_images[idx] = df.iloc[idx]["timestamp"]
 
     fig = plt.figure(figsize=(3, len(critical_images) // 3 + 1))
-    for idx, key in enumerate(critical_images.keys()):
-        img = Image.open(f"{EXTRACTED_IMAGES}{key}.jpg")
+
+    backend = Path(__file__).parent
+    shutil.rmtree(f"{backend}/critical_images/{TAR_FILE_NAME}")
+    os.mkdir(f"{backend}/critical_images/{TAR_FILE_NAME}")
+
+    for idx, image_number in enumerate(critical_images.keys()):
+        img = Image.open(f"{EXTRACTED_IMAGES}{image_number}.jpg")
         fig.add_subplot(3, len(critical_images) // 3 + 1, idx + 1)
         plt.imshow(img)
         plt.axis("off")
-        plt.title(f"Timestamp: {critical_images[key]}")
+        plt.title(f"Timestamp: {critical_images[image_number]}")
 
+        shutil.copy(
+            f"{EXTRACTED_IMAGES}{image_number}.jpg",
+            f"{backend}/critical_images/{TAR_FILE_NAME}",
+        )
     plt.show()
