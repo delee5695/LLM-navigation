b22a419cd30b4130d9dfbfebdee3beb8e77f7cc1
delee5695 <delee5695@naver.com>
2024-04-03 11:10:38 -0400

    update timestamp, add route plotting

--- backend/rdp_algorithm.py
+++ backend/rdp_algorithm.py
@@ -5,9 +5,7 @@ import matplotlib.pyplot as plt
 from PIL import Image
 import rdp
 
-TAR_FILE_NAME = (
-    "training_ua-5ad6b6714d5e52c97ca53c47f38b9655_Test_3-13-occamlab-3rd_floor"
-)
+TAR_FILE_NAME = "training_ua-9843530f4d1fbbdbd0cc26bb3e655da1_HAL_lab_to_206"
 EXTRACTED_IMAGES = (
     f"backend/.cache/firebase_data/{TAR_FILE_NAME}/extracted/localization-video/"
 )
@@ -29,14 +27,18 @@ if __name__ == "__main__":
         [np.array(sublist) for sublist in df["translation"].to_numpy()]
     )
 
-    abs_positions = rdp.rdp(positions, 0.6, return_mask=True)
-
+    abs_positions = rdp.rdp(positions, 0.6)
+    abs_positions_bool = rdp.rdp(positions, 0.6, return_mask=True)
     abs_timestamp = []
     indexes = []
-    for idx, abs_position in enumerate(abs_positions):
+    for idx, abs_position in enumerate(abs_positions_bool):
         if abs_position:
 
             critical_images[idx] = df.iloc[idx]["timestamp"]
+    print(abs_positions)
+    print(abs_timestamp)
+    print(critical_images)
+    # image plotting
 
     fig = plt.figure(figsize=(3, len(critical_images) // 3 + 1))
     for idx, key in enumerate(critical_images.keys()):
@@ -44,6 +46,15 @@ if __name__ == "__main__":
         fig.add_subplot(3, len(critical_images) // 3 + 1, idx + 1)
         plt.imshow(img)
         plt.axis("off")
-        plt.title(f"Timestamp: {critical_images[key]}")
+        plt.title(f"Timestamp: {str(critical_images[key]-30)[8:10]} (seconds)")
+
+    # # route plotting
+    # x, y, z = zip(*positions)
+    # plt.plot(z, x, marker="o", linestyle="-", color="b")
+
+    # # Plot the original path
+    # x, y, z = zip(*abs_positions)
+    # plt.plot(z, x, marker="o", linestyle="-", color="r")
+    # plt.axis("equal")
 
     plt.show()
