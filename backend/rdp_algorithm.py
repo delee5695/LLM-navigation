a9eceaf517496d385e72c7224bac4e5d27611f46
delee5695 <delee5695@naver.com>
2024-04-17 12:15:00 -0400

    improve image - calculate angular velocity - calculate heading

--- backend/rdp_algorithm.py
+++ backend/rdp_algorithm.py
@@ -1,20 +1,15 @@
+from pathlib import Path
+import math
+import os
 from firebase import FirebaseDownloader
 import pandas as pd
+import shutil
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
-import shutil
-import os
-from pathlib import Path
 import rdp
 
-<<<<<<< HEAD
 TAR_FILE_NAME = "training_ua-9843530f4d1fbbdbd0cc26bb3e655da1_HAL_lab_to_206"
-=======
-TAR_FILE_NAME = (
-    "training_ua-9843530f4d1fbbdbd0cc26bb3e655da1_HAL_lab_to_206"  # don't include .tar
-)
->>>>>>> 3bf18d31b43d0c2ff8f1a0f791dff12f49a9e2cd
 EXTRACTED_IMAGES = (
     f"backend/.cache/firebase_data/{TAR_FILE_NAME}/extracted/localization-video/"
 )
@@ -32,6 +27,7 @@ if __name__ == "__main__":
     ]["poses"]
 
     df = pd.DataFrame(localization_phase_list)
+
     positions = np.array(
         [np.array(sublist) for sublist in df["translation"].to_numpy()]
     )
@@ -40,39 +36,76 @@ if __name__ == "__main__":
     abs_positions_bool = rdp.rdp(positions, 0.6, return_mask=True)
     abs_timestamp = []
     indexes = []
+
     for idx, abs_position in enumerate(abs_positions_bool):
         if abs_position:
 
             critical_images[idx] = df.iloc[idx]["timestamp"]
-    print(abs_positions)
-    print(abs_timestamp)
-    print(critical_images)
-    # image plotting
+    # print(abs_positions)
+    # print(abs_timestamp)
+    # print(critical_images)
 
-    fig = plt.figure(figsize=(3, len(critical_images) // 3 + 1))
+    degree = {}
 
-    backend = Path(__file__).parent
-    shutil.rmtree(f"{backend}/critical_images/{TAR_FILE_NAME}")
-    os.mkdir(f"{backend}/critical_images/{TAR_FILE_NAME}")
+    for idx, num in enumerate(critical_images.keys()):
 
-    for idx, image_number in enumerate(critical_images.keys()):
-        img = Image.open(f"{EXTRACTED_IMAGES}{image_number}.jpg")
-        fig.add_subplot(3, len(critical_images) // 3 + 1, idx + 1)
-        plt.imshow(img)
-        plt.axis("off")
-        plt.title(f"Timestamp: {str(critical_images[image_number]-30)[8:10]} (seconds)")
+        if idx != len(critical_images) - 1:
+            min_rotation = 10
+            min_idx = num
+            for i in range(num, num + 120):
+                rotation_matrix0 = (
+                    np.array(df.iloc[i]["rotation_matrix"]).reshape((4, 4)).T
+                )
+                rotation_matrix1 = (
+                    np.array(df.iloc[i + 1]["rotation_matrix"]).reshape((4, 4)).T
+                )
+                heading = rotation_matrix0[:3, 2]  # b3 vector
 
-    # # route plotting
-    # x, y, z = zip(*positions)
-    # plt.plot(z, x, marker="o", linestyle="-", color="b")
+                relative_rot = (
+                    np.linalg.inv(rotation_matrix0[:3, :3]) @ rotation_matrix1[:3, :3]
+                )
+                rdp_b3 = df.iloc[num]["rotation_matrix"][8:11]
+                pointing_vec = np.array(df.iloc[num + 1].translation) - np.array(
+                    df.iloc[num].translation
+                )
 
-    # # Plot the original path
-    # x, y, z = zip(*abs_positions)
-    # plt.plot(z, x, marker="o", linestyle="-", color="r")
-    # plt.axis("equal")
+                print(
+                    np.arccos(
+                        np.dot(pointing_vec, -heading)
+                        / (np.linalg.norm(pointing_vec) * np.linalg.norm(-1 * heading))
+                    )
+                )
+                print(np.arccos((np.trace(relative_rot) - 1) / 2))
 
-        shutil.copy(
-            f"{EXTRACTED_IMAGES}{image_number}.jpg",
-            f"{backend}/critical_images/{TAR_FILE_NAME}",
-        )
-    plt.show()
+            breakpoint()
+
+    # # image plotting
+
+    # fig = plt.figure(figsize=(3, len(degree) // 3 + 1))
+
+    # backend = Path(__file__).parent
+    # shutil.rmtree(f"{backend}/critical_images/{TAR_FILE_NAME}")
+    # os.mkdir(f"{backend}/critical_images/{TAR_FILE_NAME}")
+
+    # for idx, image_number in enumerate(degree.keys()):
+    #     img = Image.open(f"{EXTRACTED_IMAGES}{image_number}.jpg")
+    #     fig.add_subplot(3, len(degree) // 3 + 1, idx + 1)
+    #     plt.imshow(img)
+    #     plt.axis("off")
+    #     # plt.title(f"Timestamp: {str(degree[image_number]-30)[8:10]} (seconds)")
+    #     plt.title(f"degree: {degree[image_number]}")
+    #     shutil.copy(
+    #         f"{EXTRACTED_IMAGES}{image_number}.jpg",
+    #         f"{backend}/critical_images/{TAR_FILE_NAME}",
+    #     )
+    # plt.show()
+    #     # # route plotting
+    #     # x, y, z = zip(*positions)
+    #     # plt.plot(z, x, marker="o", linestyle="-", color="b")
+
+    #     # # Plot the original path
+    #     # x, y, z = zip(*abs_positions)
+    #     # plt.plot(z, x, marker="o", linestyle="-", color="r")
+    #     # plt.axis("equal")
+
+    #
